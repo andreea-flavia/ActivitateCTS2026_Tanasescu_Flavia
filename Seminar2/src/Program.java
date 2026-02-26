1b64ce700850a780307367a174b4a889f1f2dacb
andreea_flavia <109135947+andreea-flavia@users.noreply.github.com>
2026-02-26 17:19:32 +0200

    Seminar 2 26.02.2026

---
+++ Seminar2/src/Program.java
@@ -0,0 +1,25 @@
+import java.io.FileNotFoundException;
+import java.util.List;
+
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.Aplicant;
+import ro.ase.cts.loaders.AngajatiLoader;
+import ro.ase.cts.loaders.AplicantiLoader;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Aplicant> listaAngajati;
+		AplicantiLoader angLoader = new AngajatiLoader();
+		try {
+			listaAngajati = angLoader.loadAplicanti("angajati.txt");
+			for(Aplicant angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
